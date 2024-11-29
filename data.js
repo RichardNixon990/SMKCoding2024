bce24e0a46d472fd2e4bb9a652671174c57b3066
RichardNixon990 <richardnixon990@gmail.com>
2024-11-29 15:01:34 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    khodam: "Raja SIGMA",
+    deskripsi:
+      "Raja SIGMA adalah khodam Ohio dengan kekuatan sangat perkasa. Raja Sigma dilindungi kekuatan gyatt yang didapat dari Kai Cenat. Hati Hati menggunakan khodam anda",
+  },
+  {
+    khodam: "Roger Sumatra",
+    deskripsi:
+      "Roger Sumatra merupakan khodam yang hanya ktif dimalam hari dan ketika pengguna merasa putus asa dan poengguna akan berubah menjadi khodamnya",
+  },
+  {
+    khodam: "Satria Macan",
+    deskripsi:
+      "Satria Macan adalah khodam yang memadukan keberanian macan dengan kebijaksanaan seorang satria, siap melindungi dan memberikan kekuatan pada penggunanya.",
+  },
+  {
+    khodam: "Garuda Aura",
+    deskripsi:
+      "Garuda Aura adalah khodam yang membawa energi dan kekuatan Garuda, memberikan perlindungan spiritual dan kebijaksanaan yang melampaui batas-batas manusia.",
+  },
+  {
+    khodam: "Rimba Singa",
+    deskripsi:
+      "Rimba Singa adalah khodam yang memiliki kekuatan dan keberanian seekor singa hutan, siap memberikan kekuatan dan ketegasan pada penggunanya.",
+  },
+  {
+    khodam: "Agus Tiger",
+    deskripsi:
+      "Agus Tiger adalah khodam yang memanfaatkan kecepatan dan ketangkasan harimau untuk memberikan perlindungan dan keberanian ekstra pada penggunanya.",
+  },
+  {
+    khodam: "Naga Jaya",
+    deskripsi:
+      "Naga Jaya adalah khodam yang memancarkan keberanian dan kekuatan naga, siap membimbing pengguna melalui perjalanan spiritual yang penuh dengan keajaiban.",
+  },
+  {
+    khodam: "Panglima Gajah",
+    deskripsi:
+      "Panglima Gajah adalah khodam yang memadukan kebijaksanaan gajah dan kekuatan seorang panglima, siap memberikan ketenangan dan kekuatan kepada penggunanya.",
+  },
+  {
+    khodam: "Kijang Sakti",
+    deskripsi:
+      "Kijang Sakti adalah khodam yang memiliki kecepatan dan kebijaksanaan kijang, siap memberikan perlindungan dan kebijaksanaan pada penggunanya dalam situasi apapun.",
+  },
+  {
+    khodam: "Buaya Agung",
+    deskripsi:
+      "Buaya Agung adalah khodam yang menggabungkan kekuatan dan ketangkasan buaya, siap memberikan perlindungan dan keberanian pada penggunanya.",
+  },
+  {
+    khodam: "Kelinci Arif",
+    deskripsi:
+      "Kelinci Arif adalah khodam yang memanfaatkan kecepatan dan kecerdasan kelinci untuk membantu pengguna menemukan solusi dalam setiap situasi.",
+  },
+  {
+    khodam: "Lembu Sakti",
+    deskripsi:
+      "Lembu Sakti adalah khodam yang memiliki kekuatan dan ketangkasan lembu, siap memberikan kekuatan dan kesabaran ekstra pada penggunanya dalam menghadapi ujian hidup.",
+  },
+  {
+    khodam: "Rizal Kalimantan",
+    deskripsi:
+      "Sosok Khodam Rizal Kalimantan menguasai air dan kegelapan malam dengan kecepatan dan ketangkasan yang ahli",
+  },
+];
+
+window.data = data;
